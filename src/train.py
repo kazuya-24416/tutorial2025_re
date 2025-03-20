91b431ca585944928199237409e50e4d24e9bc96
Kazuya <kazukane0109@gmail.com>
2025-03-20 01:42:49 +0000

    ✨ sft trainer

--- src/train.py
+++ src/train.py
@@ -1,101 +1,81 @@
-import warnings
+import json
 from pathlib import Path
 
-import bitsandbytes as bnb
 import pandas as pd
-import torch
-import transformers
 import yaml
 from datasets import Dataset
-from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training
-from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from trl import DataCollatorForCompletionOnlyLM, SFTConfig, SFTTrainer
 
+# Load configuration from config.yaml
 with Path("config/config.yaml").open() as file:
     config = yaml.safe_load(file)
 
+# Extract configuration values
 model_name_or_path = config["model_name_or_path"]
-rank = config["rank"]
-bnb_config = BitsAndBytesConfig(
-    load_in_4bit=config["bnb_config"]["load_in_4bit"],
-    bnb_4bit_use_double_quant=config["bnb_config"]["bnb_4bit_use_double_quant"],
-    bnb_4bit_quant_type=config["bnb_config"]["bnb_4bit_quant_type"],
-    bnb_4bit_compute_dtype=torch.bfloat16,
-)
-
-warnings.filterwarnings("ignore")
-
-model = AutoModelForCausalLM.from_pretrained(
-    model_name_or_path,
-    load_in_8bit=True,
-    device_map="cuda:0",
-    trust_remote_code=True,
-    quantization_config=bnb_config,
-)
-
-tokenizer = AutoTokenizer.from_pretrained(
-    model_name_or_path,
-)
-
-model = prepare_model_for_kbit_training(model)
+dataset_path = config["dataset_path"]
+response_template = config["response_template"]
 
 
-def find_all_linear_names(model: torch.nn.Module, bits: int) -> list[str]:
-    """Find all linear names in the model.
+# Load dataset
+def read_jsonlines(file_path: str) -> pd.DataFrame:
+    """Read a jsonlines file and convert it to a pandas DataFrame.
 
     Args:
-        model (torch.nn.Module): The model to find linear names in.
-        bits (int): The number of bits to use for the linear names.
+        file_path (str): Path to the jsonlines file.
 
     Returns:
-        list[str]: A list of linear names in the model.
+        pd.DataFrame: DataFrame containing the data from the jsonlines file.
 
     """
-    cls = (
-        bnb.nn.Linear4bit
-        if bits == 4
-        else (bnb.nn.Linear8bitLt if bits == 8 else torch.nn.Linear)
-    )
-    lora_module_names = set()
-    for name, module in model.named_modules():
-        if isinstance(module, cls):
-            names = name.split(".")
-            lora_module_names.add(names[0] if len(names) == 1 else names[-1])
-
-    return list(lora_module_names)
+    data = []
+    with Path(file_path).open(encoding="utf-8") as f:
+        data.extend([json.loads(line.strip()) for line in f])
+    return pd.DataFrame(data)
 
 
-target_modules = find_all_linear_names(model, 4)
+dataset = read_jsonlines(dataset_path)
+dataset = Dataset.from_pandas(dataset)
 
-lora_config = LoraConfig(
-    r=rank,
-    lora_alpha=rank * 2,
-    target_modules=target_modules,
-    lora_dropout=config["lora_dropout"],
-    bias=config["lora_bias"],
-    task_type=config["task_type"],
+# Load model and tokenizer
+model = AutoModelForCausalLM.from_pretrained(model_name_or_path)
+tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
+
+
+def formatting_prompts_func(example: dict) -> list:
+    """Format prompts for training.
+
+    Args:
+        example (dict): 1つの例のデータ.
+
+    Returns:
+        list: フォーマット済みのテキストのリスト.
+
+    """
+    output_texts = []
+    for i in range(len(example["instruction"])):
+        text = f"""
+        ### Instruction: {example["instruction"][i]}\n
+        ### Answer: {example["output"][i]}"""
+        output_texts.append(text)
+    return output_texts
+
+
+# Create data collator with response template from config
+collator = DataCollatorForCompletionOnlyLM(response_template, tokenizer=tokenizer)
+
+# Create SFTConfig from training arguments in config
+training_args = config["training_args"]
+args = SFTConfig(**training_args)
+
+
+trainer = SFTTrainer(
+    model,
+    train_dataset=dataset,
+    eval_dataset=dataset,
+    args=args,
+    formatting_func=formatting_prompts_func,
+    data_collator=collator,
 )
 
-model = get_peft_model(model, lora_config)
-
-
-data = pd.read_csv(config["data_csv_path"])
-data = data.sample(frac=1).reset_index(drop=True)
-
-data = Dataset.from_pandas(data)
-data = data.map(
-    lambda samples: tokenizer(samples[config["data_column_name"]]), batched=True
-)
-
-tokenizer.pad_token = tokenizer.eos_token
-
-trainer = transformers.Trainer(
-    model=model,
-    train_dataset=data,
-    args=transformers.TrainingArguments(**config["training_arguments"]),
-    data_collator=transformers.DataCollatorForLanguageModeling(tokenizer, mlm=False),
-)
-model.config.use_cache = False
 trainer.train()
-
-model.save_pretrained(config["output_dir"])
-tokenizer.save_pretrained(config["output_dir"])
